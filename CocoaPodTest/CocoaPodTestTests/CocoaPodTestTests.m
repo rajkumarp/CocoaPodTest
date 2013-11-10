eb19c43a839fdfb42fac17ae317dd5e60221442c
rajkumarp <Raj@Rajkumars-MacBook-Pro.local>
2013-11-09 21:55:23 -0800

    Testing CocoaPod

---
+++ CocoaPodTest/CocoaPodTestTests/CocoaPodTestTests.m
@@ -0,0 +1,34 @@
+//
+//  CocoaPodTestTests.m
+//  CocoaPodTestTests
+//
+//  Created by Rajkumar Pachaiyappan on 11/9/13.
+//  Copyright (c) 2013 tringapps, inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CocoaPodTestTests : XCTestCase
+
+@end
+
+@implementation CocoaPodTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
